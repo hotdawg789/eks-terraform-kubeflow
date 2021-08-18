93b596568f6c3829261d145c6b1cd849fb6fbb9a
CD <38231409+hotdawg789@users.noreply.github.com>
2021-08-18 16:19:50 -0400

    Update set_env_variables.sh

--- aws-terraform/set_env_variables.sh
+++ aws-terraform/set_env_variables.sh
@@ -1,2 +1,4 @@
 export CONFIG_EKS_CLUSTER_NAME="kubeflow_cluster"
 export AWS_CLUSTER_REGION="us-east-1"
+# NEED below variable set OR kfctl will fail on mainfest apply
+export AWS_DEFAULT_REGION="us-east-1"
