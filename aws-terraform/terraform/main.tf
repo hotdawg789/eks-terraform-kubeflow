6003eaa6af0da3e5a2cc32aed055ae007726f4d2
CD <38231409+hotdawg789@users.noreply.github.com>
2021-08-18 13:27:55 -0400

    terraform var update

--- aws-terraform/terraform/main.tf
+++ aws-terraform/terraform/main.tf
@@ -11,8 +11,6 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
 }
 
 data "aws_caller_identity" "current" {}
--- aws-terraform/terraform/variables.tf
+++ aws-terraform/terraform/variables.tf
@@ -1,19 +1,8 @@
 variable "aws_region" {
   type        = string
   description = "AWS Region where resources should be created"
+  default     = "us-east-1"
 }
-
-variable "aws_access_key" {
-  type        = string
-  description = "AWS Access Key"
-}
-
-
-variable "aws_secret_key" {
-  type        = string
-  description = "AWS Secret Key"
-}
-
 
 variable "aws_cluster_name" {
   type        = string
