754057cf4ec2d4ee6070f1c0062b3e11f4b58fcf
CD <38231409+hotdawg789@users.noreply.github.com>
2021-09-30 09:09:40 -0400

    Update deploy_kubeflow.sh

--- aws-terraform/deploy_kubeflow.sh
+++ aws-terraform/deploy_kubeflow.sh
@@ -28,27 +28,27 @@ export KUBECONFIG="$PWD/terraform/kubeconfig_$CONFIG_EKS_CLUSTER_NAME"
 echo "KUBECONFIG=$KUBECONFIG"
 kubectl get nodes --all-namespaces
 
-# Prepare for Kubeflow deployment
-export KUBEFLOW_DIR="$PWD/$CONFIG_EKS_CLUSTER_NAME"
-export CONFIG_URI="https://raw.githubusercontent.com/kubeflow/manifests/v1.2-branch/kfdef/kfctl_aws.v1.2.0.yaml"
-export CONFIG_FILE="$KUBEFLOW_DIR/kfctl_aws.yaml"
+# # Prepare for Kubeflow deployment
+# export KUBEFLOW_DIR="$PWD/$CONFIG_EKS_CLUSTER_NAME"
+# export CONFIG_URI="https://raw.githubusercontent.com/kubeflow/manifests/v1.2-branch/kfdef/kfctl_aws.v1.2.0.yaml"
+# export CONFIG_FILE="$KUBEFLOW_DIR/kfctl_aws.yaml"
 
-# Go to working directory and download kfctl_aws.yaml
-mkdir -p $KUBEFLOW_DIR && cd $KUBEFLOW_DIR
-wget -O $CONFIG_FILE $CONFIG_URI
+# # Go to working directory and download kfctl_aws.yaml
+# mkdir -p $KUBEFLOW_DIR && cd $KUBEFLOW_DIR
+# wget -O $CONFIG_FILE $CONFIG_URI
 
-# Replace region in kfctl_aws.yaml
-sed -i -e "s/us-west-2/$AWS_CLUSTER_REGION/g" $CONFIG_FILE
-sed -i -e "s/#roles/roles/g" $CONFIG_FILE
-sed -i -e "s/#- eksctl-kubeflow-aws-nodegroup-ng-a2-NodeInstanceRole-xxxxxxx/- $TERRAFORM_EKS_CLUSTER_IAM_ROLE_NAME/g" $CONFIG_FILE
-sed -i -e "s/enablePodIamPolicy: true/enablePodIamPolicy: false/g" $CONFIG_FILE
-tail -15 $CONFIG_FILE
+# # Replace region in kfctl_aws.yaml
+# sed -i -e "s/us-west-2/$AWS_CLUSTER_REGION/g" $CONFIG_FILE
+# sed -i -e "s/#roles/roles/g" $CONFIG_FILE
+# sed -i -e "s/#- eksctl-kubeflow-aws-nodegroup-ng-a2-NodeInstanceRole-xxxxxxx/- $TERRAFORM_EKS_CLUSTER_IAM_ROLE_NAME/g" $CONFIG_FILE
+# sed -i -e "s/enablePodIamPolicy: true/enablePodIamPolicy: false/g" $CONFIG_FILE
+# tail -15 $CONFIG_FILE
 
-# Pray
-kfctl apply -V -f $CONFIG_FILE
+# # Pray
+# kfctl apply -V -f $CONFIG_FILE
 echo "Waiting for all services to be ready (2 min)" && sleep 2m
 
 kubectl get service istio-ingressgateway -n istio-system
 # If you want to use LoadBalancer instead of NodePort
 # kubectl patch svc istio-ingressgateway -p '{"spec": {"type": "LoadBalancer"}}' -n istio-system
-kubectl port-forward svc/istio-ingressgateway -n istio-system 8080:80
+# kubectl port-forward svc/istio-ingressgateway -n istio-system 8080:80
